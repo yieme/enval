41cd1e3ac4d3cda399063260e1de104759b8bc08
yieme <yieme@cormea.com>
2017-06-01 17:31:52 -0600

    addl test cases

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ function assert(ok, message) { // minimal assert, no need for a complex library
 var envic = require('./')
 
 var val = envic('TEST', undefined, true)
-
+console.log(val)
 assert(val !== undefined, 'TEST should exist');
 assert(val.foo, '"foo" should exist');
 assert(val.foo == 'bar', '"foo" should equal "bar"')
@@ -51,3 +51,15 @@ console.log('')
 val = envic('TEST9', undefined, true)
 assert(val !== undefined, 'TEST9 should exist');
 assert(val == 'a:b,c:d,e', 'TEST9 should equal "a:b,c:d,e" as it is malformed JSON and JSONIC')
+
+console.log('')
+val = envic('TEST10', undefined, 1)
+assert(val === 'Hi', 'TEST10 should be "Hi"');
+
+console.log('')
+val = envic('TEST11', undefined, function(v) { return v })
+assert(val === 'Yo', 'TEST11 should be "Yo"');
+
+console.log('')
+val = envic('TEST_FN', undefined, function(v) { return 'No' })
+assert(val === 'Yes', 'TEST_FN should be "Yes"');
